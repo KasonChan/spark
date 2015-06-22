8e42cf8c24f6aa48acffe4dfbc2349dce99f73f9
KasonChan <kasonl.chan@gmail.com>
2015-06-22 14:56:32 -0500

    Add demo

--- build.sbt
+++ build.sbt
@@ -3,4 +3,6 @@ name := "Spark"
 version := "1.0"
 
 scalaVersion := "2.11.6"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "1.4.0"
     
---
+++ src/main/scala/demo/Demo.scala
@@ -0,0 +1,20 @@
+package demo
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * Created by kasonchan on 6/22/15.
+ */
+object Demo {
+
+  def main(args: Array[String]) {
+    val logFile = "/Users/kasonchan/IdeaProjects/Spark/README.md" // Should be some file on your system
+    val conf = new SparkConf().setAppName("Simple Application").setMaster("local[2]")
+    val sc = new SparkContext(conf)
+    val logData = sc.textFile(logFile, 2).cache()
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b")).count()
+    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+  }
+
+}
