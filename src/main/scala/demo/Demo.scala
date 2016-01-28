be69d7c20b0b53521d5c11a73e582d450e886e7f
kasonchan <kasonl.chan@gmail.com>
2016-01-28 17:24:59 +0800

    Update Demo and .gitignore; Delete Read; Add build.properties and plugins.sbt

--- src/main/scala/demo/Demo.scala
+++ src/main/scala/demo/Demo.scala
@@ -2,19 +2,26 @@ package demo
 
 import org.apache.spark.{SparkConf, SparkContext}
 
+import scala.util.{Failure, Success, Try}
+
 /**
- * Created by kasonchan on 6/22/15.
- */
+  * Created by kasonchan on 6/22/15.
+  */
 object Demo {
 
   def main(args: Array[String]) {
-    val logFile = "resources/data/README.md" // Should be some file on your system
-    val conf = new SparkConf().setAppName("Simple Application").setMaster("local[2]")
-    val sc = new SparkContext(conf)
-    val logData = sc.textFile(logFile, 2).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
-    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+    Try {
+      val logFile = "/Users/kasonchan/IdeaProjects/Spark/README.md" // Should be some file on your system
+      val conf = new SparkConf().setAppName("Simple Application").setMaster("local[2]")
+      val sc = new SparkContext(conf)
+      val logData = sc.textFile(logFile, 2).cache()
+      val numAs = logData.filter(line => line.contains("a")).count()
+      val numBs = logData.filter(line => line.contains("b")).count()
+      println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+    } match {
+      case Success(s) =>
+      case Failure(f) => println(f)
+    }
   }
 
 }
